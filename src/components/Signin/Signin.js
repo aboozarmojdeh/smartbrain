735401daa40dd28a16d5b8f43de7505497662c14
aboozarmojdeh <aboozar.mojdeh@gmail.com>
2020-08-23 14:56:34 -0400

    serve package added to package.json

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -22,7 +22,7 @@ class Signin extends Component {
   }
 
   onSubmitSignin=()=>{
-    fetch('http://localhost:3000/signin',{
+    fetch('https://smart-brain-aboozar.herokuapp.com/signin',{
       method:'post',
       headers:{'Content-Type':'application/json'},
       body:JSON.stringify({
